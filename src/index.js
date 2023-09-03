82c38cf04f2798451f6f3af2ab0dd6997e4c5511
melissasidisid <moimelly451@gmail.com>
2023-09-03 22:02:31 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let addlist=document.getElementById('addlist');
+let toDocontainet=document.getElementById('todoContainer');
+let inpuField=document.getElementById('inputField');
+
+addlist.addEventListener('click',function(){
+    let list=document.createElement('list');
+    list.innerText=inputField.value;
+
+    toDocontainet.appendChild(list)
+})
+
+
+document.links[0].onclick= function(event){
+
+
+    console.log(event)
+    event.preventDefault();
+  
+  }
+ 
 });
